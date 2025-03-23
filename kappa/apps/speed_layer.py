cc68e085c6ade24593cf0bc92982061c8579bbce
Mathéo Raharison <matheo.raharison@imt-atlantique.net>
2025-03-23 15:59:27 +0100

    Ajout de traitements en stream :     - 2 | Calcul du nombre de docks et vélos disponibles au total sur le réseau vélozef     - 3 | Calcul du nombre de stations fonctionnelles (qui permettent de déposer et réserver un vélo)

--- kappa/apps/speed_layer.py
+++ kappa/apps/speed_layer.py
@@ -1,17 +1,164 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, window, sum, to_timestamp, lit
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType
+from pyspark.sql.functions import col, from_json, window, sum, to_timestamp, count, date_format
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType, BooleanType
 from cassandra.cluster import Cluster
+from threading import Thread
 
+# Configuration constants
 IP_CASSANDRA_NODE = "172.22.0.4"
+KAFKA_BOOTSTRAP_SERVERS = "kafka:29092"
+TOPIC_STATION_STATUS = 'station_status'
+TOPIC_STATION_INFORMATION = 'station_information'
+TOPIC_BIKE_STATUS = 'free_bike_status'
+KEYSPACE_NAME = 'velozef'
+CHECKPOINT_LOCATION = "/tmp/checkpoints"
+
+
+def create_spark_session():
+    return SparkSession.builder \
+        .appName("Spark-Kafka-Cassandra") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1") \
+        .config("spark.cassandra.connection.host", IP_CASSANDRA_NODE) \
+        .getOrCreate()
+
+def create_cassandra_tables(session):
+    keyspace_query = f'''
+    CREATE KEYSPACE IF NOT EXISTS {KEYSPACE_NAME} WITH replication = {{
+        'class' : 'SimpleStrategy',
+        'replication_factor' : 1
+    }};'''
+    
+    station_status_query = f'''
+    CREATE TABLE IF NOT EXISTS velozef.station_status_windowed (
+    window_start TIMESTAMP,
+    window_end TIMESTAMP,
+    empty_count INT,
+    full_count INT,
+    saturation_count INT,
+    total_docks_available INT,
+    total_bikes_available INT,
+    PRIMARY KEY ((window_start), window_end) 
+    ) WITH CLUSTERING ORDER BY (window_end DESC);  
+    '''
+
+    station_information_query = f'''
+    CREATE TABLE IF NOT EXISTS {KEYSPACE_NAME}.station_information (
+        station_id TEXT PRIMARY KEY,
+        capacity INT,
+        name TEXT
+    );'''
+
+    bike_status_query = f'''
+    CREATE TABLE IF NOT EXISTS {KEYSPACE_NAME}.bike_status (
+        window_start TIMESTAMP,
+        window_end TIMESTAMP,
+        abandoned_bikes_count INT,
+        PRIMARY KEY ((window_start, window_end))
+    );'''
+
+    session.execute(keyspace_query)
+    session.execute(station_status_query)
+    session.execute(station_information_query)
+    session.execute(bike_status_query)
+
+def read_kafka_stream(spark, topic):
+    return spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
+        .option("subscribe", topic) \
+        .option("startingOffsets", "earliest") \
+        .option("failOnDataLoss", "false") \
+        .load()
+
+def stream_to_df(stream, schema):
+    return stream.selectExpr("CAST(value AS STRING)") \
+                 .select(from_json(col("value"), schema).alias("data")) \
+                 .select("data.*")
+
+def process_station_status(df_station_status, df_capacity):
+    """Traitements : 
+    - 1 | Calcul du nombre de stations pleines/vides/proche de saturation par fenêtre de 60 secondes
+    - 2 | Calcul du nombre de docks et vélos disponibles au total sur le réseau vélozef
+    - 3 | Calcul du nombre de stations fonctionnelles (qui permettent de déposer et réserver un vélo)
+    """
+    df_station_status = df_station_status.withColumn("timestamp", to_timestamp(col("last_reported")))
+    df_joined = df_station_status.join(df_capacity, "station_id")
+    
+    # 1 | Calcul du nombre de stations pleines/vides/proche de saturation par fenêtre de 60 secondes
+    required_columns = ["num_bikes_available", "capacity"]
+    if all(column in df_joined.columns for column in required_columns):
+        df_processed = df_joined.withColumn("is_full", (col("num_bikes_available") / col("capacity") == 1).cast("int")) \
+                               .withColumn("saturation", (col("num_bikes_available") >= col("capacity") - 3).cast("int")) \
+                               .withColumn("is_empty", (col("num_bikes_available") == 0).cast("int"))
+
+
+    else:
+        raise ValueError(f"Les colonnes requises {required_columns} ne sont pas toutes présentes dans le DataFrame.")
+
+    windowed_df = df_processed \
+                    .withWatermark("timestamp", "2 minutes") \
+                    .groupBy(window(col("timestamp"), "60 seconds")) \
+                    .agg(sum(col("num_bikes_available")).alias("total_bikes_available"),
+                        sum(col('num_docks_available')).alias("total_docks_available"),
+                        sum(col("is_empty")).alias("empty_count"),
+                        sum(col("is_full")).alias("full_count"),
+                        sum(col("saturation")).alias("saturation_count")) \
+                    .withColumn("window_start", date_format(col("window.start"), "yyyy-MM-dd HH:mm:ss")) \
+                    .withColumn("window_end", date_format(col("window.end"), "yyyy-MM-dd HH:mm:ss")) \
+                    .drop("window")
+
+    return windowed_df
+
+def process_abandoned_bikes(df_bike_status):
+    """Traitement des vélos abandonnés."""
+    df_bike_status = df_bike_status.withColumn("timestamp", to_timestamp(col("last_reported")))
+    
+    # Filtrer les vélos abandonnés (station_id vide)
+    df_abandoned = df_bike_status.filter(col("station_id") == "")
+    
+    # Compter les vélos abandonnés par fenêtre de 60 secondes
+    abandoned_df = df_abandoned \
+                            .withWatermark("timestamp", "2 minutes") \
+                            .groupBy(window(col("timestamp"), "60 seconds")) \
+                            .agg(count("*").alias("abandoned_bikes_count")) \
+                            .withColumn("window_start", col("window.start")) \
+                            .withColumn("window_end", col("window.end")) \
+                            .drop("window")
+    
+    return abandoned_df
+
+def write_to_cassandra_station(writeDF, epochId):
+    """Write station status data to Cassandra."""
+    writeDF.write \
+        .format("org.apache.spark.sql.cassandra") \
+        .mode('append') \
+        .options(table="station_status_windowed", keyspace=KEYSPACE_NAME) \
+        .save()
+
+def write_to_cassandra_bike(writeDF, epochId):
+    """Write abandoned bikes data to Cassandra."""
+    writeDF.write \
+        .format("org.apache.spark.sql.cassandra") \
+        .mode('append') \
+        .options(table="bike_status", keyspace=KEYSPACE_NAME) \
+        .save()
 
 def main():
-    # Définir les schémas
+    # Init Spark
+    spark = create_spark_session()
+    spark.sparkContext.setLogLevel("ERROR")
+    # Init Cassandra
+    cluster = Cluster([IP_CASSANDRA_NODE])
+    session = cluster.connect()
+    create_cassandra_tables(session)
+
+    # Definition des schemas
     stationstatusSchema = StructType([
         StructField("station_id", StringType(), False),
         StructField("num_bikes_available", IntegerType(), False),
         StructField("num_docks_available", IntegerType(), False),
-        StructField("last_reported", IntegerType(), False)  # Utiliser last_reported comme timestamp
+        StructField("last_reported", IntegerType(), False),
     ])
 
     stationinformationSchema = StructType([
@@ -20,140 +167,63 @@ def main():
         StructField("name", StringType(), False),
     ])
 
-    # Initialiser Spark
-    spark = SparkSession.builder \
-        .appName("Spark-Kafka-Cassandra") \
-        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1") \
-        .config("spark.cassandra.connection.host", IP_CASSANDRA_NODE) \
-        .getOrCreate()
+    bikestatusSchema = StructType([
+        StructField("bike_id", StringType(), False),
+        StructField("station_id", StringType(), False),
+        StructField("last_reported", IntegerType(), False)
+    ])
 
-    spark.sparkContext.setLogLevel("ERROR")
+    # Read Kafka
+    station_status_stream = read_kafka_stream(spark, TOPIC_STATION_STATUS)
+    station_information_stream = read_kafka_stream(spark, TOPIC_STATION_INFORMATION)
+    bike_status_stream = read_kafka_stream(spark, TOPIC_BIKE_STATUS)
 
-    # Définir les topics Kafka
-    topic_station_status = 'station_status'
-    topic_station_information = 'station_information'
-    
-    ################ SESSION CASSANDRA #################
-
-    clstr = Cluster([str(IP_CASSANDRA_NODE)])
-    session = clstr.connect()
-    keyspace_qry = ''' CREATE KEYSPACE IF NOT EXISTS velozef WITH replication = {
-    'class' : 'SimpleStrategy',
-    'replication_factor' : 1
-    }  ;'''
-    
-    # Create a new cassandra table
-    station_status_qry = '''
-    CREATE TABLE IF NOT EXISTS velozef.station_status_windowed (
-    window_start TIMESTAMP,
-    window_end TIMESTAMP,
-    empty_count INT,
-    full_count INT,
-    saturation_count INT,
-    PRIMARY KEY ((window_start,window_end))
-    );'''
-
-    station_information_qry = '''
-    CREATE TABLE IF NOT EXISTS velozef.station_information (
-    station_id TEXT PRIMARY KEY,
-    capacity INT,
-    name TEXT);'''
-
-    session.execute(keyspace_qry)
-    session.execute(station_status_qry)
-    session.execute(station_information_qry)
-
-    ################# LECTURE FLUX KAFKA #################
-    def read_kafka_stream(topic):
-        return spark \
-            .readStream \
-            .format("kafka") \
-            .option("kafka.bootstrap.servers", "kafka:29092") \
-            .option("subscribe", topic) \
-            .option("startingOffsets", "earliest") \
-            .load()
-    # Transformer les flux en DataFrame structuré
-    def stream_to_df(stream, schema):
-        return stream.selectExpr("CAST(value AS STRING)") \
-                    .select(from_json(col("value"), schema).alias("data")) \
-                    .select("data.*")
-    
-    # Lire les flux Kafka
-    station_status_stream = read_kafka_stream(topic_station_status)
-    station_information_stream = read_kafka_stream(topic_station_information)
-
-    # Transformer les flux en DataFrames
+    # Stream -> df
     df_station_status = stream_to_df(station_status_stream, stationstatusSchema)
     df_station_information = stream_to_df(station_information_stream, stationinformationSchema)
+    df_bike_status = stream_to_df(bike_status_stream, bikestatusSchema)
 
-    # Convertir last_reported UNIX en timestamp
-    df_station_status = df_station_status.withColumn("timestamp", to_timestamp(col("last_reported")))
-
-    # Écrire les informations de capacité dans Cassandra 
-    """
-    Il était impossible de réaliser une jointure entre les deux tables lues sur kafka et 
-    d'écrire le résultat du traitement (qui utilisait <capacity> de station information et les attributs de station_status) en mode update, d'où l'idée d'écrire la table station_information qui ne contient que des informations statiques de toute manière.
-    """
+    # Stocke les informations de stations_information dans une table Cassandra
+    print("Stockage des informations de station_information dans Cassandra...")
+    # write df_station_information in console
+    # df_station_information.writeStream \
+    #     .outputMode("append") \
+    #     .format("console") \
+    #     .start()
+    
     df_station_information.writeStream \
         .format("org.apache.spark.sql.cassandra") \
-        .option("keyspace", "velozef") \
+        .option("keyspace", KEYSPACE_NAME) \
         .option("table", "station_information") \
         .option("checkpointLocation", "/tmp/checkpoints/station_information") \
         .outputMode("append") \
         .start()
 
-    # Lire les informations de capacité depuis Cassandra
     df_capacity = spark.read \
         .format("org.apache.spark.sql.cassandra") \
-        .option("keyspace", "velozef") \
+        .option("keyspace", str(KEYSPACE_NAME)) \
         .option("table", "station_information") \
         .load()
 
-    ############## TRAITEMENTS #################
-    #1 | Compter le nombre de stations vides, remplies, proche de saturation
+    ##### TRAITEMENTS #####
+    windowed_df = process_station_status(df_station_status, df_capacity)
+    abandoned_df = process_abandoned_bikes(df_bike_status)
 
-    # Joindre les DataFrames
-    df1 = df_station_status.join(df_capacity, "station_id")
-
-    # Calculer les colonnes supplémentaires
-    required_columns = ["num_bikes_available", "capacity"]
-    if all(column in df1.columns for column in required_columns):
-        df2 = df1.withColumn("is_full", (col("num_bikes_available") / col("capacity") == 1).cast("int")) \
-                .withColumn("saturation", (col("num_bikes_available") >= col("capacity") - 3).cast("int")) \
-                .withColumn("is_empty", (col("num_bikes_available") == 0).cast("int"))
-    else:
-        raise ValueError(f"Les colonnes requises {required_columns} ne sont pas toutes présentes dans le DataFrame.")
-
-    # Appliquer une fenêtre de 60 secondes basée sur last_reported
-    windowed_df = df2.groupBy(
-        window(col("timestamp"), "60 seconds")  # Fenêtre de 60 secondes                    
-    ).agg(
-        sum(col("is_empty")).alias("empty_count"),     
-        sum(col("is_full")).alias("full_count"),   
-        sum(col("saturation")).alias("saturation_count")  
-    )
-
-    # Extraire window_start et window_end
-    windowed_df = windowed_df.withColumn("window_start", col("window.start")) \
-                            .withColumn("window_end", col("window.end")) \
-                            .drop("window")  
-
-    
-    ################## ECRITURE DANS CASSANDRA #####################
-    windowed_df.printSchema()
-    def writeToCassandra(writeDF, epochId):
-        writeDF.write \
-            .format("org.apache.spark.sql.cassandra")\
-            .mode('append')\
-            .options(table="station_status_windowed", keyspace="velozef")\
-            .save()
 
     windowed_df.writeStream \
-        .option("spark.cassandra.connection.host", IP_CASSANDRA_NODE)\
-        .foreachBatch(writeToCassandra) \
-        .outputMode("update") \
-        .start()\
-        .awaitTermination()
+    .option("spark.cassandra.connection.host", IP_CASSANDRA_NODE)\
+    .foreachBatch(write_to_cassandra_station) \
+    .option("checkpointLocation", CHECKPOINT_LOCATION) \
+    .outputMode("update")\
+    .start()\
+    .awaitTermination()
+
+    # abandoned_df.writeStream \
+    #     .foreachBatch(write_to_cassandra_bike) \
+    #     .option("checkpointLocation", CHECKPOINT_LOCATION) \
+    #     .outputMode("update") \
+    #     .start()
+
 
 if __name__ == "__main__":
     main()
